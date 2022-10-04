61c60e651284b2049817fd7d667fdc41f38ca5a4
juandzapata <juan.zapata22871@ucaldas.edu.co>
2022-10-04 16:28:29 -0500

    Se crean los servicios para enviar 'email' y 'sms'

---
+++ servidor.py
@@ -0,0 +1,73 @@
+# d-230f67b8e83449ad95965ac4bfb65907 Id plantilla de Registro Usuario
+
+# save this as app.py
+import os
+from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Adventure Park!"
+
+
+@app.route("/email", methods=['POST'])
+def email():
+    hash = request.form['hash_validator']
+    if (hash == os.environ.get('HASH_VALIDATOR')):
+        try:
+            email_sender = os.environ.get("EMAIL_SENDER")
+            to = request.form['destination']
+            subject = request.form['subject']
+            message_content = request.form['message']
+            message = Mail(
+                from_email=email_sender,
+                to_emails=to,
+                subject=subject,
+                html_content=message_content)
+            try:
+                sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+                return "OK"
+            except Exception as e:
+                print(e)
+                return "KO"
+        except:
+            return "Faltan datos para el mensaje"
+    else:
+        return "Hash Error"
+
+
+@app.route("/sms", methods=['POST'])
+def sms():
+    hash = request.form['hash_validator']
+    print(os.environ.get('HASH_VALIDATOR'))
+    if (hash == os.environ.get('HASH_VALIDATOR')):
+        try:
+            account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+            auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+            print("esta es la variable de entorno", account_sid)
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                messaging_service_sid=os.environ.get(
+                    'TWILIO_MESSAGING_SERVICE_SID'),
+                body=request.form['Primer mensaje de texto'],
+                to=request.form['+573104572663']
+            )
+            print(message.sid)
+            return "enviado"
+        except:
+            return "error no enviado"
+    else:
+        return "error de hash"
+
+
+if __name__ == '__main__':
+    app.run()
